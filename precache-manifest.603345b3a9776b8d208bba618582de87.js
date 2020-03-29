f3df1c26b2ec528d42c9d6eaeb14ebdd6ddc811f
Snehangshu Chakraborty <chakraborty.snehangshu@gmail.com>
2020-03-29 10:56:55 +0530

    Updates

---
+++ precache-manifest.603345b3a9776b8d208bba618582de87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "417f23be398bd35b92d8",
+    "url": "/monster-rolodex/static/css/main.c5513984.chunk.css"
+  },
+  {
+    "revision": "417f23be398bd35b92d8",
+    "url": "/monster-rolodex/static/js/main.4c72c455.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  },
+  {
+    "revision": "27567163bda5e8f4c8e3",
+    "url": "/monster-rolodex/static/js/2.6a720388.chunk.js"
+  },
+  {
+    "revision": "ed9ca5405331cf5b8e08c56c39170bb1",
+    "url": "/monster-rolodex/index.html"
+  }
+];
